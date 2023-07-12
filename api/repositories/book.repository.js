d6b28521cd37a27d8865c95be2f2cb1c55970f19
mwenski <mwenski@outlook.com>
2023-07-12 19:26:49 +0200

    book.repository.js added

---
+++ api/repositories/book.repository.js
@@ -0,0 +1,71 @@
+const { connect } = require('../config/db.config');
+
+class BookRepository {
+    db = {};
+
+    constructor(){
+        this.db = connect;
+        this.db.sequelize.sync({ force: true }).then(() => {
+            console.log("Drop and re-sync db");
+        });
+    }
+
+    async getBooks(){
+        try{
+            const books = await this.db.books.findAll();
+            console.log("books: ", books);
+            return books;
+        }catch(err){
+            console.log(err);
+            return [];
+        }
+    }
+
+    async createBook(book){
+        let data = {};
+
+        try{
+            book.createdate = new Date().toISOString();
+            data = await this.db.books.create(book);
+        }catch(err){
+            console.log("Error: ", err);
+        }
+
+        return data;
+    }
+
+    async updateBook(book){
+        let data = {};
+
+        try{
+            book.updatedate = new Date().toISOString();
+            data = await this.db.books.update({...book}, {
+                where: {
+                    id: book.id
+                }
+            });
+        }catch(err){
+            console.log("Error: ", err);
+        }
+
+        return data;
+    }
+
+    async deleteBook(bookId){
+        let data = {}
+
+        try{
+            data = await this.db.books.destroy({
+                where: {
+                    id: bookId
+                }
+            });
+        }catch(err){
+            console.log("Error: ", err);
+        }
+
+        return data;
+    }
+}
+
+module.exports = new BookRepository();
--- api/services/book.service.js
+++ api/services/book.service.js
@@ -1,22 +1,23 @@
+const bookRepository = require('../repositories/book.repository');
+
 class BookService{
 
     constructor(){}
 
-
     async getBooks(){
-
+        return await bookRepository.getBooks();
     }
 
     async createBook(book){
-
+        return await bookRepository.createBook(book);
     }
 
     async updateBook(book){
-
+        return await bookRepository.updateBook(book);
     }
 
     async deleteBook(bookId){
-
+        return await bookRepository.deleteBook(bookId);
     }
 
 }
