2bd221f65d3954efd3f0a512c16df7681533092d
mwenski <mwenski@outlook.com>
2023-07-25 18:57:05 +0200

    react router added to ui

---
+++ ui/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import '../App.css';
+
+function Home(){
+    return (
+        <p>
+            TEST
+        </p>
+    );
+}
+
+export default Home;
