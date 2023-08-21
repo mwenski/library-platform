fd053f019468c06f7c4cb0120ff5df8f1c79628c
mwenski <mwenski@outlook.com>
2023-08-21 20:09:22 +0200

    header menu was made responsive (ui)

--- ui/src/components/global/Header.jsx
+++ ui/src/components/global/Header.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LuLibrary } from "react-icons/lu";
+import { BiMenu } from "react-icons/bi";
 
 function Header(){
+    const [ulExpanded, setUlExpanded] = useState(false);
+
     return (
         <header>
             <div className="logo">
@@ -10,12 +14,16 @@ function Header(){
                     <div>library</div>
                 </Link>
             </div>
-            <ul>
+            <button onClick={() => setUlExpanded(!ulExpanded)}>
+                <BiMenu />
+            </button>
+            <ul className={ulExpanded ? "expanded" : ""}>
                 <li><Link to="/">Catalog</Link></li>
                 <li><Link to="/borrowers">Borrowers</Link></li>
                 <li><Link to="/login">Sign in</Link></li>
                 <li><Link to="/register">Sign up</Link></li>
             </ul>
+            
         </header>
     )
 }
